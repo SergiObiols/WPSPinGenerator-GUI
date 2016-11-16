f841ce68883f2ab00d37cda59320dff2b2204490
root <root@localhost.localdomain>
2016-11-16 23:33:38 +0100

    Solved issue getting monitor mode name

--- src/main.py
+++ src/main.py
@@ -8,5 +8,5 @@ def createWindow(networkInterface):
 	height = mainWindow.winfo_screenheight()
 	mainWindow.geometry(str(width) + "x" + str(height))
 	mainWindow.title("WPSPinGenerator-GUI")
-	airmonInterface = os.popen("airmon-ng start "+networkInterface+" | awk '{print $5}' | tail -n 2").read()[:-3]
+	airmonInterface = os.popen("airmon-ng start "+networkInterface+" | grep monitor | awk '{print $5}'").read()[:-2]
 	print(airmonInterface)
